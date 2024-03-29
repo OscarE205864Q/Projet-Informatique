8ec7d17ed03935c1aff19e2e20725e49a295d0cc
OscarE205864Q <163477001+OscarE205864Q@users.noreply.github.com>
2024-03-29 01:08:22 +0100

    Update dijkstra.jl

--- algorithmes/dijkstra.jl
+++ algorithmes/dijkstra.jl
@@ -1,96 +1,64 @@
-# Importation d'un module pour utiliser des structures de données avancées
+include("../config.jl")
+include("../defs.jl")
+
+# Importation d'un module pour utiliser un tas binaire
 using DataStructures
 
-# Structure de noeud pour l'algorithme de Dijkstra
-struct Node
-    position::Tuple{Int, Int}
-    distance::Int
-end
-
-# Fonction pour initialiser la file de priorité avec les distances
-function initialiser_file(grille, depart)
-    file = PriorityQueue{Node, Int}()
-    distances = Dict{Tuple{Int, Int}, Int}()
-
-    for i in 1:size(grille, 1), j in 1:size(grille, 2)
-        distances[(i, j)] = 10000  # On prend un nombre très grand
-    end
-
-    distances[depart] = 0
-    enqueue!(file, Node(depart, 0), 0)
-
-    return file, distances
-end
-
-# Fonction pour reconstruire le chemin depuis l'arrivée
-function chemin_optimal(depart, arrivee, case_actuelle, precedents)
-    chemin = []
-    case_actuelle = arrivee
-
-    while case_actuelle != depart
-        pushfirst!(chemin, case_actuelle)
-
-        # Si la case d'arrivée n'est pas dans le dictionnaire, c'est qu'on ne l'a jamais atteinte
-        if !(case_actuelle in collect(keys(precedents)))
-            return []
-        end
-
-        case_actuelle = precedents[case_actuelle]
-    end
-
-    pushfirst!(chemin, depart)
-
-    return chemin
-end
-
 # Algorithme de Dijkstra
-function dijkstra_algo(grille, depart, arrivee)
-    # Initialisation des structures de données
-    file, distances = initialiser_file(grille, depart)
-    precedents = Dict{Tuple{Int, Int}, Tuple{Int, Int}}()
+function dijkstra_algo(map::Matrix{Char}, start::Tuple{Int64, Int64}, finish::Tuple{Int64, Int64})
+	# Tuple de la hauteur et largeur de la carte
+    map_size = size(map)
+	seen_count = 0
 
-    # Initialisation de case_actuelle pour une utilisation sur tout le scope de la fonction
-    case_actuelle = depart
+	# Création d'un tas binaire de (distance au point de départ, position) ordonné par distance au point de départ
+	heap::MutableBinaryHeap{Tuple{Int64, Tuple{Int64, Int64}}} = MutableBinaryHeap{Tuple{Int64, Tuple{Int64, Int64}}}(Base.By(first))
+	res = push!(heap, (0, start))
 
-    while !isempty(file)
-        node = dequeue!(file)
-        case_actuelle, distance_actuelle = node.position, node.distance
-        # Si on atteint l'arrivée, on arrête la boucle
-        if case_actuelle == arrivee
-            break
-        end
+	# Création d'un dictionnaire de (position => position, longueur du chemin, indice du tas binaire) pour stocker les cases visitées et recréer le chemin final
+	nodes_dict = Dict(start => NodeData((-1, -1), 0, res))
 
-        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)]
-            suivant = (case_actuelle[1] + dx, case_actuelle[2] + dy)
+	# Tant qu'il reste des cases dans le tas, on cherche
+	while !isempty(heap)
+		# Récupération de la distance totale et de la case courantes
+		total_distance, node = pop!(heap)
 
-            # On teste si la case vers laquelle on essaye d'aller est valide
-            if 1 <= suivant[1] <= size(grille, 1) && 1 <= suivant[2] <= size(grille, 2) && grille[suivant[1], suivant[2]] == 0
-                nouvelle_distance = distance_actuelle + 1
+		# On regarde si on a atteint la case finale
+		if node == finish
+			break
+		end
 
-                # On met à jour le dictionnaire des distances
-                if nouvelle_distance < distances[suivant]
-                    distances[suivant] = nouvelle_distance
-                    enqueue!(file, Node(suivant, nouvelle_distance), nouvelle_distance)
-                    precedents[suivant] = case_actuelle
-                end
-            end
-        end
-    end
+		# On itère sur les cases voisines pour progresser
+		for next in [node .+ (1, 0), node .+ (-1, 0), node .+ (0, 1), node .+ (0, -1)]
+			# On vérifie que la case voisine est atteignable
+			if is_reachable(map_size, next, map[next[1], next[2]])
+				# Récupération du "coût de mouvement" pour se déplacer à la case voisine
+				movement_cost = get(MOVEMENT_COST, map[next[1], next[2]], DEFAULT_MOVEMENT_COST)
 
-    return chemin_optimal(depart, arrivee, case_actuelle, precedents)
+				# On calcule la nouvelle distance totale entre le point de départ et la case voisine
+				new_distance = nodes_dict[node].total_distance + movement_cost
+
+				# Si on a pas encore visité la case voisine, on la "recense", sinon on met à jour sa distance associée si besoin
+				if !haskey(nodes_dict, next)
+					res = push!(heap, (new_distance, next))
+					nodes_dict[next] = NodeData(node, total_distance + movement_cost, res)
+					seen_count += 1
+				elseif new_distance < nodes_dict[next].total_distance
+					update!(heap, nodes_dict[next].heap_index, (new_distance, next))
+				end
+			end
+		end
+	end
+
+	# On retourne une liste contenant la taille du chemin, le nombre de cases visitées, et le chemin, s'il existe
+	if !(finish in keys(nodes_dict))
+		return [0, seen_count, []]
+	end
+
+	return [nodes_dict[finish].total_distance, seen_count, get_rebuilt_path(nodes_dict, finish)]
 end
 
-# Grille avec obstacles (1)
-grille = [
-    0 0 0 0 0;
-    0 1 1 1 0;
-    0 1 0 1 0;
-    0 1 1 1 0;
-    0 0 0 0 0
-]
+starting_time = time()
+res = dijkstra_algo(get_map_matrix(MAP_PATH), START, FINISH)
+elapsed_time = round(time() - starting_time, digits=4)
 
-depart = (1, 1)
-arrivee = (3, 3)
-
-chemin = dijkstra_algo(grille, depart, arrivee)
-println("Chemin le plus court : ", chemin)
+print_solution(res, elapsed_time)
